54153d50fc7c789bba55c111e3df859b16b3ea56
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-01-28 06:07:20 -0500

    Add createPlayer() factory function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,25 @@
-console.log("Script linked.");
+function createPlayer(name, marker) {
+    // Private variables
+    let score = 0;
+
+    // Public functions
+    const getName = () => name;
+    const getMarker = () => marker;
+    const win = () => ++score;
+    const getScore = () => score;
+
+    return {
+        getName,
+        getMarker,
+        win,
+        getScore
+    };
+}
+
+// Test code
+const ted = createPlayer("ted", "X");
+const jill = createPlayer("jill", "O");
+jill.win();
+jill.win();
+console.log(`${ted.getMarker()} (${ted.getName()}): ${ted.getScore()}`);
+console.log(`${jill.getMarker()} (${jill.getName()}): ${jill.getScore()}`);
